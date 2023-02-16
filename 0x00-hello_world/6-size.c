94cab22e7bdbb8017645dd493e8b89d5abe741de
adiomi90 <adiomi90@outlook.com>
2023-02-16 14:56:21 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - entry 
-* 
-* Return: success
-*/
+ * main - Entry
+ *
+ * Return: Success
+ */
 int main(void)
 {
 	int a;
@@ -12,7 +12,6 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
-
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
