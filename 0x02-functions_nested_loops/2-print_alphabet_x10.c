860b5da3435b14853b78a7e1f792a477eca12c09
adiomi90 <adiomi90@outlook.com>
2023-02-21 10:17:13 -0800

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alpahabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter;
+	while (counter < 10)
+	{
+	char letter; 
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	counter++;
+	}
+}
