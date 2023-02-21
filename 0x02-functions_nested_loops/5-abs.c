a6230f601e10cbbb26ae6ed3e756445adecc0246
adiomi90 <adiomi90@outlook.com>
2023-02-21 11:24:44 -0800

    abs

---
+++ 0x02-functions_nested_loops/5-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - check the absolute value
+ *@n: number to be checked
+ *Return: 0 or 1 
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
