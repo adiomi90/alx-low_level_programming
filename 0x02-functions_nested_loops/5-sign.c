212044ab0b70b7d491eed9d8c824feb2fc36c93c
adiomi90 <adiomi90@outlook.com>
2023-02-21 11:18:27 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ * @n: the number to be checked
+ * Return: + for positive,0 for zero,- for negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+
+	}
+}
+
