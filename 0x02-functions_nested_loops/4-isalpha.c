964cfb65235374b77558bfa144e319c926eb7e0b
adiomi90 <adiomi90@outlook.com>
2023-02-21 10:59:31 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if its is letter, lowercase or uppercase
+ * @c: letter to be checked
+ * Return: 1 for true , 0 for false
+ */
+
+int _isalpha(int c)
+{
+	for ((c >='a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
