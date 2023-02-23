2da16cf44ec8b27fe1ea91e43fe84ccd113ec4c2
adiomi90 <adiomi90@outlook.com>
2023-02-23 13:13:49 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - start of the function
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+	i = 1;
+	while ( i < 10)
+	{
+		_putchar(i);
+		i++;
+
+	}
+}
