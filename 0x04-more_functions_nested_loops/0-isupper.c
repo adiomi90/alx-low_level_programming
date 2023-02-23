175a191a34b7618afb3b8d6f796036bb25da5129
adiomi90 <adiomi90@outlook.com>
2023-02-23 08:43:00 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,19 @@
-#include "stdio.h"
+#include <stdio.h>
 #include "main.h"
-/**
- * _isupper - function name 
- * @c:character to check
- * Return:0
- */
 
+/**
+ * _isupper - entry point
+ * @c: character to check
+ * Return: 0
+*/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
