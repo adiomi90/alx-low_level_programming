36a0570f446ec5eb4dfe5b1e3b27fed1529e6ef5
adiomi90 <adiomi90@outlook.com>
2023-02-23 14:52:46 -0800

    print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * print_most_numbers - print numbers
- */
+* print_most_numbers - prints 0-9 with a new line, excludes 2 and 4
+* Description: Uses headers to link and a nested loops to achieve goal
+* Return: void. no return.
+*/
+
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = '0'; i < '10'; i++)
-	{
-		if(( i == '2') && (i == '4'))
-		{
-			continue;
-		}
-		_putchar(i);
-		i++;
-	}
+	for (i = 0; i < 10; i++)
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
 	_putchar('\n');
 }
