074a6853bce881280942164428dc0e4250128040
adiomi90 <adiomi90@outlook.com>
2023-02-27 13:56:40 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: int to check
+ * Return: 0 is success
+ */
+void print_rev(char *s)
+{
+	int string = 0;
+
+	for (string = 0; s[string] != '\0'; string++)
+		;
+	for (string--; string >= 0; string--)
+		_putchar(s[string]);
+
+	_putchar('\n');
+}
