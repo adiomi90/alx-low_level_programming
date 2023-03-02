1cf2dd073cc1d10701702c0c33decd8e2b743036
adiomi90 <adiomi90@outlook.com>
2023-03-02 11:17:49 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,8 @@
  * @src: The source string to be appended to @dest.
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
