3b2c1b271b1dc986a87fbac2cc87bcd4b2bdca0f
adiomi90 <adiomi90@outlook.com>
2023-03-02 12:08:12 -0800

    cap string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,6 @@ char *_strncat(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
-cha *cap_string(char *);
+cha *cap_string(char *s);
 
 #endif
