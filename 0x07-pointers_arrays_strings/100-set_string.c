7d31e0532c51da1d293c05920f01420cc8180a28
adiomi90 <adiomi90@outlook.com>
2023-03-06 15:23:39 -0800

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function to set the pointer
+ * @s: address
+ * #to: address
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
