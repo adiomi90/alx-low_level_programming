9310441006e9e2c3b771e7ec1474dd82964847e5
adiomi90 <adiomi90@outlook.com>
2023-03-08 14:38:06 -0800

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* is_palindrome - Entry point
+* Description - A function that takes a pointer to an int
+* *@s: the function accepts an input saved into s
+* Return: Nothing for now
+*/
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
