95704802cbc5ef467b2b82736f4c536fae5738a2
adiomi90 <adiomi90@outlook.com>
2023-03-18 00:05:28 -0700

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -7,5 +9,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _sum(char **s, unsigned int l);
+char *_multiplyrev(char *s, char c, int o);
+int _isnumber(char *s);
+unsigned int _lenght(char *s);
+void _puts(char *s);
 
 #endif /*MAIN_H*/
