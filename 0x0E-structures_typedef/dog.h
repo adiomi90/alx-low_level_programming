86b0bf587647c8452041b0b7a4f2e92c518a5bd0
adiomi90 <adiomi90@outlook.com>
2023-03-20 13:09:52 -0700

    dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dogs
+ * @name: name of dog 
+ * @age:  age of dog
+ * @owner: owner of pet
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
