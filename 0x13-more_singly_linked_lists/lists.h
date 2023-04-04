c29ab42fd41c0189624c099b4e335960967d3673
adiomi90 <adiomi90@outlook.com>
2023-04-04 14:59:51 -0700

    print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,12 +15,24 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
+size_t print_listint(const listint_t *h);
 
 
 #endif /*LISTS_H*/
