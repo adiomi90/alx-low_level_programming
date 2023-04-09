6be73b7782b741d7d186b79c5ad6cb0234ddf5f5
adiomi90 <adiomi90@outlook.com>
2023-04-08 17:42:25 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,23 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0, mult = 1;
-	int len;
+	int len, i;
 
 	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
+
+	}
+	
+	len -= 1;
+
+
+	for (i = 0; b[len]; i++)
+	{
 
 		num += (b[len] - '0') * mult;
 		mult *= 2;
