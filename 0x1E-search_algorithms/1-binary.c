35830fabb9c4aa7266228308e4ef732e061ae1bc
adiomi90 <adiomi90@outlook.com>
2023-12-13 00:32:25 -0800

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -29,7 +29,7 @@ void print_array(int *array, size_t left, size_t right)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t mid = 0;
+	size_t middle = 0;
 	size_t left = 0;
 	size_t right = size - 1;
 
@@ -41,7 +41,7 @@ int binary_search(int *array, size_t size, int value)
 		print_array(array, left, right);
 		middle = (left + right) / 2;
 		if (array[middle] == value)
-			return (mid);
+			return (middle);
 		else if (array[middle] > value)
 			right = middle - 1;
 		else
