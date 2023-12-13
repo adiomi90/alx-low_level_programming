d708890474d844664f5ff57bdea94b6f7f3d98c4
adiomi90 <adiomi90@outlook.com>
2023-12-13 02:57:11 -0800

    exponential algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ int binary_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+int _binary_search(int *array, size_t left, size_t right, int valuei);
 
 #endif /*SEARCH_ALGOS*/
