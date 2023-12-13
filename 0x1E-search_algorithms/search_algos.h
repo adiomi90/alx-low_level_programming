8fa75e116c636a9aa687fe2d8392953a47634a91
adiomi90 <adiomi90@outlook.com>
2023-12-13 15:26:12 -0800

    advance algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -43,12 +43,13 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t left, size_t right);
 int interpolation_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
 int binary_search_helper(int *array, size_t left, size_t right, int value);
+int _binary_search(int *array, size_t left, size_t right, int value)
 int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 listint_t *jump_list((listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
