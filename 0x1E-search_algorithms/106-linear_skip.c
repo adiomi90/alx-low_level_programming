8d16194a22abdd2622f21444f9520e8cead07b2e
adiomi90 <adiomi90@outlook.com>
2023-12-13 18:26:44 -0800

    linear skip

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,40 +1,44 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - using linear skip algorithm to search in singly
- * 		 linked list
- * @list: the head pointer
- * @value: the value to search for
- * Return: a pointer to the first node value or NULL
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ * @list: Pointer to the head of the skip list to search in.
+ * @value: The value to search for.
+ *
+ * Return: Pointer to the first node where the value is located, or NULL if not found.
  */
-
-
-skiplist_t *linear_skip(skiplist_t *list, int value)
-{
-    skiplist_t *current = list, *express;
+skiplist_t *linear_skip(skiplist_t *list, int value) {
+    skiplist_t *current, *express;
 
     if (list == NULL)
         return NULL;
 
+    current = list;
     express = list->express;
 
-    while (express != NULL && express->n < value) {
+    while (express != NULL) {
         printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-        current = express;
-        express = express->express;
-    }
 
-    printf("Value found between indexes [%lu] and [%lu]\n", current->index, express ? express->index : 0);
+        if (express->n >= value || express->express == NULL) {
+            printf("Value found between indexes [%lu] and [%lu]\n",
+                   current->index, express->index);
 
-    while (current != NULL && current->index < express->index) {
-        printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+            while (current != NULL && current->index <= express->index) {
+                printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
 
-        if (current->n == value) {
-            printf("Found %d at index: %lu\n", value, current->index);
-            return current;
+                if (current->n == value) {
+                    printf("Found %d at index: %lu\n", value, current->index);
+                    return current;
+                }
+
+                current = current->next;
+            }
+
+            break;  /* Value not found or found in regular lane */
         }
 
-        current = current->next;
+        current = express;
+        express = express->express;
     }
 
     printf("Value not found in the skip list\n");
